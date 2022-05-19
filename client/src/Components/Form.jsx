5c0f14a8c5be3a8839259cde9ce2cf7d4c56ea4d
Lorene Chew <lorene.chew@gmail.com>
2022-05-18 20:15:46 -0700

    added css

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -2,6 +2,17 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import {openAIKey} from '../../../config';
 import { useMainContext } from './MainProvider.jsx';
+import styled from 'styled-components';
+
+const StyledForm = styled.form `
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+const StyledSubmit = styled.input `
+  background-color: rgba(195, 234, 255, 0.8);
+`;
 
 function Form(props) {
   const { prompt, setPrompt, allPromptsAndResponses, setAllPromptsAndResponses } = useMainContext();
@@ -15,60 +26,52 @@ function Form(props) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    async function postData() {
-      try {
-        let request = await axios.post('https://api.openai.com/v1/engines/text-curie-001/completions', prompt, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${openAIKey}`,
-            'Access-Control-Allow-Origin' : 'https://confluence-open-api.herokuapp.com',
-            'Access-Control-Allow-Methods':'GET, POST'
-          }
-        })
-        if (request.status === 200) {
-          console.log(request)
-          console.log('prompt: ', prompt)
-          let data = allPromptsAndResponses;
-          data.push([[prompt.prompt, `${request.data.choices[0].text}`]])
-          setAllPromptsAndResponses(data);
-          setPrompt({prompt: ''});
-        }
-      } catch(err) {
-        console.log(err);
-      }
-    }
-    postData();
-
-    // axios.post('https://api.openai.com/v1/engines/text-curie-001/completions', prompt, {
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Authorization': `Bearer ${openAIKey}`
-    //   }
-    // })
-    // .then((res) => {
-    //   console.log('res.data: ', res.data)
-    //   let data = allPromptsAndResponses;
-    //       data.push([prompt.prompt, `${res.data.choices[0].text}`])
+    // async function postData() {
+    //   try {
+    //     let request = await axios.post('https://api.openai.com/v1/engines/text-curie-001/completions', prompt, {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //         'Authorization': `Bearer ${openAIKey}`,
+    //         'Access-Control-Allow-Origin' : 'https://confluence-open-api.herokuapp.com',
+    //         'Access-Control-Allow-Methods':'GET, POST'
+    //       }
+    //     })
+    //     if (request.status === 200) {
+    //       console.log(request)
+    //       console.log('prompt: ', prompt)
+    //       let data = allPromptsAndResponses;
+    //       data.push([[prompt.prompt, `${request.data.choices[0].text}`]])
     //       setAllPromptsAndResponses(data);
-    //   // setAllPrompts(prevArray => {
-    //   //   [...prevArray, prompt.prompt]
-    //   // })
-    //   setPrompt({prompt: ''});
-    //   // let responses = allResponses;
-    //   // responses.push(res.data.choices[0].text)
-    //   // setAllResponses(responses);
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // });
+    //       setPrompt({prompt: ''});
+    //     }
+    //   } catch(err) {
+    //     console.log(err);
+    //   }
+    // }
+    // postData();
+
+    axios.post('https://api.openai.com/v1/engines/text-curie-001/completions', prompt, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${openAIKey}`
+      }
+    })
+    .then((res) => {
+      let data = allPromptsAndResponses;
+      data.push([[prompt.prompt, `${res.data.choices[0].text}`]])
+      setAllPromptsAndResponses(data);
+      setPrompt({prompt: ''});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
   return (
-
     <>
-      <form onSubmit={handleSubmit}>
-        <input type='text' name='prompt' placeholder='Enter text' value={prompt.prompt} onChange={handleChange} />
-        <input type='submit' />
-      </form>
+      <StyledForm onSubmit={handleSubmit}>
+        <textarea type='text' name='prompt' placeholder='Enter text' value={prompt.prompt} onChange={handleChange} />
+        <StyledSubmit type='submit' />
+      </StyledForm>
     </>
   )
 }
--- client/src/Components/PromptsAndResponsesEntry.jsx
+++ client/src/Components/PromptsAndResponsesEntry.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(255, 245, 179, 0.8);
+  border-bottom: dotted;
+`;
+
+const StyledContent = styled.div `
+  display: flex;
+  flex-direction: row;
+  margin-top: 5px;
+`;
+
+const StyledHeader = styled.div `
+  font-weight: bold;
+  margin-right: 3px;
+`;
+
+const StyledData = styled.div `
+  font-family: fantasy;
+`;
 
 function PromptsAndResponsesEntry({ entry }) {
   return (
-    <>
-      <div>
-        Prompt: {entry.props.set[0][0]}
-      </div>
-      <div>
-        Response: {entry.props.set[0][1]}
-      </div>
-      <br/>
-    </>
+    <StyledContainer>
+      <StyledContent>
+        <StyledHeader>Prompt:</StyledHeader>
+        <StyledData>{entry.props.set[0][0]}</StyledData>
+      </StyledContent>
+      <StyledContent>
+        <StyledHeader>Response:</StyledHeader>
+        <StyledData>{entry.props.set[0][1]}</StyledData>
+      </StyledContent>
+    </StyledContainer>
   )
 }
 
